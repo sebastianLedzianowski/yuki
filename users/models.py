8b3118e9705f22c73916fd44505ff97ca310d62f
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2024-06-20 18:42:30 +0200

    update users

--- users/models.py
+++ users/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
-from workshop.models import phone_validator
+from utils.validators import phone_validator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(validators=[phone_validator], max_length=9, null=True, blank=True)
-    avatar = models.ImageField(default='avatar.jpg', upload_to='profile_avatars')
+    avatar = models.ImageField(default='avatar.png', upload_to='profile_avatars')
 
     def __str__(self):
         return f'{self.user.username} Profile'
