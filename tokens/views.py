f27a97f57828cf96be3cb8ed81fe759a359a2fea
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2024-06-20 20:24:02 +0200

    add create_def

--- tokens/views.py
+++ tokens/views.py
@@ -3,9 +3,8 @@ from django.contrib import messages
 from django.utils import timezone
 from django.views import View
 
+from utils.create_def import create_access_token, create_subscription
 from workshop.models import Workshop
-from .models import Subscription, AccessToken
-from utils.jwt_tokens import generate_jwt_token
 
 
 class SubscriptionView(View):
@@ -13,6 +12,7 @@ class SubscriptionView(View):
         workshop = get_object_or_404(Workshop, pk=kwargs['pk'], owner=request.user)
         subscription = getattr(workshop, 'subscription', None)
         tokens = subscription.tokens.all() if subscription else []
+
         return render(request, 'tokens/subscription_detail.html', {
             'workshop': workshop,
             'subscription': subscription,
@@ -22,33 +22,22 @@ class SubscriptionView(View):
 
     def post(self, request, *args, **kwargs):
         workshop = get_object_or_404(Workshop, pk=kwargs['pk'], owner=request.user)
-        subscription = getattr(workshop, 'subscription', None)
+        subscriptions = getattr(workshop, 'subscription', None)
 
-        if subscription:
-            if subscription.is_active:
+        if subscriptions:
+            if subscriptions.is_active:
+
                 messages.info(request, 'The workshop already has an active subscription.')
             else:
-                subscription.end_date = timezone.now() + timezone.timedelta(days=30)
-                subscription.save()
-                AccessToken.objects.create(
-                    token=generate_jwt_token(workshop.id),
-                    expiration_date=timezone.now() + timezone.timedelta(days=30),
-                    created_at=timezone.now(),
-                    subscription=subscription
-                )
+                subscriptions.end_date = timezone.now() + timezone.timedelta(days=30)
+                subscriptions.save()
+                create_access_token(subscriptions, days_valid=30, minutes_valid=0)
+
                 messages.success(request, 'Subscription reactivated successfully.')
         else:
-            subscription = Subscription.objects.create(
-                start_date=timezone.now(),
-                end_date=timezone.now() + timezone.timedelta(days=30),
-                workshop=workshop,
-            )
-            AccessToken.objects.create(
-                token=generate_jwt_token(workshop.id),
-                expiration_date=timezone.now() + timezone.timedelta(days=30),
-                created_at=timezone.now(),
-                subscription=subscription,
-            )
+            subscription = create_subscription(workshop, days_valid=30, minutes_valid=0)
+            create_access_token(subscription, days_valid=30, minutes_valid=0)
+
             messages.success(request, 'Subscription purchased successfully.')
 
         return redirect('subscription_detail', pk=workshop.pk)
